854686fa01b97faa7b1afea7d16766bf6bf3bfa9
Ariyadas Bathushan <ariyadasbathushan@gmail.com>
2025-03-09 23:10:15 +0530

    added photo

--- lib/models/Product.dart
+++ lib/models/Product.dart
@@ -63,7 +63,7 @@ List<Product> products = [
     price: 234,
     size: 12,
     description: dummyText,
-    image: "assets/images/dogfood_6.png",
+    image: "assets/images/dogfood_5.png",
     color: const Color(0xFFAEAEAE),
 
     
